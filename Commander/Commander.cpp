58ff56bbade9a23c8a52494abec3cd2bab6ab70c
Kalhar Pandya <kalharpandya38@gmail.com>
2021-07-26 14:00:04 +0530

    Smart Commander

--- Commander/Commander.cpp
+++ Commander/Commander.cpp
@@ -1,14 +1,47 @@
-#define maxLines  100
-class Line{
-    public:
+#define maxLines 100
+
+class Line
+{
+public:
     int steps;
+    int orignalSteps = 0;
     Direction speed;
+    Direction modifiedSpeed;
+
+    void set(Direction _speed, int _steps)
+    {
+        speed.fx = _speed.fx;
+        speed.fy = _speed.fy;
+        speed.fr = _speed.fr;
+        steps = _steps;
+        orignalSteps = _steps;
+    }
+    Direction getSpeed()
+    {
+        steps -= 1;
+        double offset;
+        if (steps > orignalSteps / 2)
+        {
+            //Incrementing
+            offset = (double)(orignalSteps - steps) / (orignalSteps / 2);
+        }
+        else
+        {
+            offset = (double)(steps) / (orignalSteps / 2);
+        }
+        modifiedSpeed.fx = speed.fx * offset;
+        modifiedSpeed.fy = speed.fy * offset;
+        modifiedSpeed.fr = speed.fr * offset;
+        return modifiedSpeed;
+    }
 };
-long meterToPulse(double meter){
-    return ((meter * 1000.0) / (0.5*3.14 * 48.0)) * 720.0;
+long meterToPulse(double meter)
+{
+    return ((meter * 1000.0) / (0.5 * 3.14 * 48.0)) * 720.0;
 }
 
-long mps(int ms, int interval = 10){
+long mps(int ms, int interval = 10)
+{
     int pulses = meterToPulse(ms);
     return (pulses / (1000 / interval));
 }
@@ -35,75 +68,80 @@ public:
         speed = _speed;
         Serial.println("Converted");
         point.display();
-        Serial.println("Speed = "+speed);
+        Serial.println("Speed = " + speed);
         computeSpeeds();
     }
     void computeSpeeds()
     {
 
         maxPoint = point.absMax();
-        if(maxPoint == 0)
+        if (maxPoint == 0)
             return;
         Serial.print("Max =");
         Serial.println(maxPoint);
-        xFactor = ((double)point.fx / maxPoint) ;
-        yFactor = ((double)point.fy / maxPoint) ;
-        rFactor = ((double)point.fr / maxPoint) ;
+        xFactor = ((double)point.fx / maxPoint);
+        yFactor = ((double)point.fy / maxPoint);
+        rFactor = ((double)point.fr / maxPoint);
 
         rationalSpeed.fx = xFactor * speed;
         rationalSpeed.fy = yFactor * speed;
         rationalSpeed.fr = rFactor * speed;
         rationalSpeed.display();
         double rmax = rationalSpeed.absMax();
-        if(rmax==0)
+        if (rmax == 0)
             return;
         steps = maxPoint / rmax;
-        Serial.println("Steps = "+ String(steps));
+        Serial.println("Steps = " + String(steps));
     }
-}lineProcessor;
+} lineProcessor;
 
 class Commander
 {
-    public:
-        Line *allLines[maxLines];
-        Direction empty;
-        int lineIndex = -1;
-        int feederIndex = -1;
-        int lineCount = 0;
-        bool available = false;
-        Commander(){
+public:
+    Line *allLines[maxLines];
+    Direction empty;
+    int lineIndex = -1;
+    int feederIndex = -1;
+    int lineCount = 0;
+    bool available = false;
+    Commander()
+    {
 
-            empty.fx = 0;
-            empty.fy = 0;
-            empty.fr = 0;
-        }
-        void addPoint(Direction target, int speed){
-            if(lineIndex>=maxLines-1){
-                Serial.println("Commander Overflow");
-                return;
-            }
-
-            available = true;
-            lineIndex += 1;
-            allLines[lineIndex] = new Line();
-
-            lineProcessor.set(target, speed);
-            allLines[lineIndex]->steps = lineProcessor.steps;
-            allLines[lineIndex]->speed = lineProcessor.rationalSpeed;
+        empty.fx = 0;
+        empty.fy = 0;
+        empty.fr = 0;
+    }
+    void addPoint(Direction target, int speed)
+    {
+        if (lineIndex >= maxLines - 1)
+        {
+            Serial.println("Commander Overflow");
+            return;
         }
 
-        Direction next(){
-            if(allLines[lineCount]-> steps > 0){
-                allLines[lineCount]->steps -= 1;
-                return allLines[lineCount]->speed;
-            }
-            if(lineCount<lineIndex){
-                lineCount += 1;
-                return next();
-            }
-            available = false;
-            return empty;
-        }
+        available = true;
+        lineIndex += 1;
+        allLines[lineIndex] = new Line();
 
+        lineProcessor.set(target, speed);
+
+        allLines[lineIndex]->set(lineProcessor.rationalSpeed, lineProcessor.steps);
+    }
+
+    Direction next()
+    {
+        if (allLines[lineCount]->steps > 0)
+        {
+            // allLines[lineCount]->steps -= 1;
+            return allLines[lineCount]->getSpeed();
+        }
+        if (lineCount < lineIndex)
+        {
+            lineCount += 1;
+            return next();
+        }
+        available = false;
+        return empty;
+    }
 
 } AutoCommander;
